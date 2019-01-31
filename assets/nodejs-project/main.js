803fac1d33a5e51e0b1f6d3853621898957f5fc2
МАА <adik.m@sgspu.ru>
2019-01-31 16:55:48 +0400

    4

--- assets/nodejs-project/main.js
+++ assets/nodejs-project/main.js
@@ -4,11 +4,13 @@ var Datastore = require('nedb');
 
 //load db
 db = {};
+//{"vtx":{"vin":{"nValue":0,"pubKey":"0","scriptSig":"0"}, "vout":{"nValue":0,"pubKey":"0"}, "txType":0, "feedback":"0", "url":"0", "token":"0", "tvalue":"0"}, "hashPrevBlock":"0", "hashMerkleRoot":"0", "hashAccountRoot":"0", "nVersion":1, "nHeight":0, "nTime":0, "nNonce":0, "_id":"0"}
 db.blocks = new Datastore({ filename: __dirname+'/db/blocks', autoload: true });
-db.txs = new Datastore({ filename: __dirname+'/db/txs', autoload: true });
 db.trie = new Datastore({ filename: __dirname+'/db/trie', autoload: true });
 db.peers = new Datastore({ filename: __dirname+'/db/peers', autoload: true });
 
+//wws - web-wallet-server
+//node - full node of coin
 const state = {
     wws: null,
     node: null,
@@ -16,6 +18,7 @@ const state = {
 };
 
 function start() {
+    //init wws & node
     state.wws = require('./wws/')().listen(3000, () => {
         console.log('Started web-wallet on 3000');
     });
@@ -23,11 +26,20 @@ function start() {
         console.log('Add socket wws', state.sockets.length + 1);
         state.sockets.push(socket);
     });
-
-    state.node.on('restart', () => {
-        console.log('Start reload fnc');
-        restart();
+    state.node = require('./node/')( () => {
+        console.log('Started node');
     });
+//    state.node.on('connection', (socket) => {
+//        console.log('Add socket node', state.sockets.length + 1);
+//        state.sockets.push(socket);
+//    });
+    //restart event
+//    state.node.on('restart', () => {
+//        console.log('Start reload fnc');
+//        restart();
+//    });
+    //msg node<--->wws:
+
 }
 
 function pathCheck(id) {
